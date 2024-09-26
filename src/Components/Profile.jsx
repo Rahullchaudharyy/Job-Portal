8ae1000c5c1ce0c637fc169206fda9e41b9528dd
Rahullchaudharyy <rahullchaudharryy@gmail.com>
2024-09-26 12:50:21 +0530

    ..

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { auth, db } from '../utils/firebase.js'; // Adjust the path as necessary
+// import { auth, db } from '../utils/firebase.js'; // Adjust the path as necessary
+import { auth,db } from '../utils/firebase.js';
 import { doc, getDoc, updateDoc } from 'firebase/firestore';
 import { useDispatch } from 'react-redux';
 import { addUser } from '../utils/userSlice';
